95d9c4ba0e35cd21a3731825d0b5cdd8e682357e
Hofsiedge <zuev.ilia.al@yandex.ru>
2020-12-20 05:01:14 +0300

    bullshit, game.game_session

--- db.sql
+++ db.sql
@@ -3,129 +3,138 @@ CREATE SCHEMA game;
 
 CREATE TABLE common.user 
 (
-    id SERIAL PRIMARY KEY,
-    pwd_hash BIGINT,
-    username TEXT,
-    email TEXT
+    id       serial PRIMARY KEY,
+    pwd_hash bigint,
+    username text,
+    email    text
 );
 
 CREATE TABLE common.group 
 (
-    id SERIAL,
-    name TEXT 
+    id   serial,
+    name text
 );
 
 CREATE TABLE common.permission
 (
-    id INTEGER PRIMARY KEY,
-    code_name TEXT
+    id        integer PRIMARY KEY,
+    code_name text
 );
 
 CREATE TABLE common.user_profile
 (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES common.user(id),
-    avatar_link TEXT,
-    status INTEGER,
-    last_seen TIMESTAMP
+    id          serial PRIMARY KEY,
+    user_id     integer REFERENCES common.user(id),
+    avatar_link text,
+    status      integer,
+    last_seen   TIMESTAMP
 );
 
 CREATE TABLE common.friend
 (
-    user_id1 INTEGER REFERENCES common.user(id),
-    user_id2 INTEGER REFERENCES common.user(id),
+    user_id1  integer REFERENCES common.user(id),
+    user_id2  integer REFERENCES common.user(id),
     direction integer,
     PRIMARY KEY (user_id1, user_id2)
 );
 
 CREATE TABLE game.game
 (
-    id BIGSERIAL PRIMARY KEY,
-    name TEXT,
-    status BOOLEAN,
-    is_active BOOLEAN,
-    owner_id INTEGER REFERENCES common.USER(id)
+    id       bigserial PRIMARY KEY,
+    name     text,
+    status   boolean,
+    owner_id integer REFERENCES common.USER(id)
 );
 
 CREATE TABLE game.character
 (
-    id BIGSERIAL PRIMARY KEY,
-    game_id BIGINT REFERENCES game.game(id),
-    user_id INTEGER REFERENCES common.user(id),
-    name text,
-    avatar_link text 
+    id          bigserial PRIMARY KEY,
+    session_id  bigint REFERENCES game.game_session(id),
+    user_id     integer REFERENCES common.user(id),
+    name        text,
+    avatar_link text
 );
 
 CREATE TABLE game.trait 
 (
-    id BIGSERIAL PRIMARY KEY,
-    game_id BIGINT REFERENCES game.game(id),
-    name text
+    id      bigserial PRIMARY KEY,
+    game_id bigint REFERENCES game.game(id),
+    name    text,
+    UNIQUE (game_id, name)
 );
 
 CREATE TABLE game.trait_value
 (
-    id BIGSERIAL,
-    trait_id BIGINT REFERENCES game.trait(id),
-    character_id BIGINT REFERENCES game.character(id),
-    value INTEGER,
+    id           bigserial,
+    trait_id     bigint REFERENCES game.trait(id),
+    character_id bigint REFERENCES game.character(id),
+    value        integer,
     PRIMARY KEY (id, trait_id)
-);
-
-CREATE TABLE common.participant
-(
-    id BIGSERIAL PRIMARY KEY,
-    game_id BIGINT REFERENCES game.game(id),
-    user_id INTEGER REFERENCES common.user(id)
 );
 
 CREATE TABLE game.map
 (
-    id           BIGSERIAL PRIMARY KEY,
+    id           bigserial PRIMARY KEY,
     name         text,
+    description  text,
     preview_link text,
-    x            FLOAT,
-    y            FLOAT,
-	pattern      INTEGER
+    x            float,
+    y            float,
+	pattern      integer
+);
+
+CREATE TABLE game.game_session
+(
+    id      bigserial PRIMARY KEY,
+    master  bigint    REFERENCES game.user(id),
+    active  boolean,
+    game_id bigint    REFERENCES game.game(id)
+);
+
+CREATE TABLE common.participant
+(
+    session_id bigint    REFERENCES game.game_session(id),
+    user_id    integer   REFERENCES common.user(id),
+    PRIMARY KEY (session_id, user_id)
 );
 
 CREATE TABLE game.map_copy
 (
-    id           BIGSERIAL,
+    id           bigserial,
+    map_id       bigint REFERENCES game.map(id),
+    session_id   bigint REFERENCES game.game_session(id),
     name         text,
     description  text,
     preview_link text,
-    game_id      BIGINT REFERENCES game.game(id),
-    map_id       BIGINT REFERENCES game.map(id),
-    x            FLOAT,
-    y            FLOAT,
+    x            float,
+    y            float,
     PRIMARY KEY (id, map_id)
 );
 
 CREATE TABLE game.effect
 (
-    id BIGSERIAL PRIMARY KEY,
-    effect_key TEXT,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP
+    id         bigserial PRIMARY KEY,
+    effect_key text,
+    start_time timestamp,
+    end_time   timestamp
 );
 
 CREATE TABLE game.effect_instance
 (
-    id BIGSERIAL PRIMARY KEY,
-    character_id BIGINT REFERENCES game.character(id),
-    effect_id BIGINT REFERENCES game.effect(id),
-    steps_left integer
+    id           bigserial PRIMARY KEY,
+    character_id bigint    REFERENCES game.character(id),
+    effect_id    bigint    REFERENCES game.effect(id),
+    steps_left   integer
 );
 
 CREATE TABLE game.area_type
 (
-    id          BIGINT,
-    map_id      BIGINT REFERENCES game.map(id),
-    map_copy_id BIGINT,
-	image_link  TEXT,
-	name        TEXT,
-	effect_id   BIGINT REFERENCES game.effect(id),
+    id          bigint,
+    map_id      bigint REFERENCES game.map(id),
+    map_copy_id bigint,
+	image_link  text,
+	name        text,
+	effect_id   bigint REFERENCES game.effect(id),
     PRIMARY KEY (map_id, map_copy_id, id),
 	FOREIGN KEY (map_id, map_copy_id) REFERENCES game.map_copy(map_id, id),
     UNIQUE      (map_id, map_copy_id, name)
@@ -133,12 +142,12 @@ CREATE TABLE game.area_type
 
 CREATE TABLE game.area
 (
-    id           BIGINT,
-    map_id       BIGINT               REFERENCES game.map(id),
-    map_copy_id  BIGINT,
-    x            INTEGER,
-    y            INTEGER,
-	area_type_id BIGINT,
+    id           bigint,
+    map_id       bigint               REFERENCES game.map(id),
+    map_copy_id  bigint,
+    x            integer,
+    y            integer,
+	area_type_id bigint,
 	FOREIGN KEY (map_id, map_copy_id) REFERENCES game.map_copy(map_id, id),
 	FOREIGN KEY (map_id, map_copy_id, area_type_id) REFERENCES game.area_type(map_id, map_copy_id, id),
 	PRIMARY KEY (map_id, map_copy_id, id),
@@ -148,25 +157,25 @@ CREATE TABLE game.area
 
 CREATE TABLE game.effect_area
 (
-	id BIGSERIAL PRIMARY KEY,
-	area_id BIGINT,
-    map_id BIGINT ,
-    map_copy_id BIGINT ,
-	effect_id BIGINT REFERENCES game.effect(id),
+	id          bigserial PRIMARY KEY,
+	area_id     bigint,
+    map_id      bigint ,
+    map_copy_id bigint ,
+	effect_id   bigint REFERENCES game.effect(id),
 	
 	FOREIGN KEY (area_id, map_id, map_copy_id) REFERENCES game.area(id, map_id, map_copy_id),
-	values FLOAT[]
+	values float[]
 );
 
 
 CREATE TABLE game.link_short
 (
-	from_area_id BIGINT,
-    from_map_id BIGINT,
-    from_map_copy_id BIGINT,
-	to_area_id BIGINT,
-    to_map_id BIGINT,
-    to_map_copy_id BIGINT,
+	from_area_id     bigint,
+    from_map_id      bigint,
+    from_map_copy_id bigint,
+	to_area_id       bigint,
+    to_map_id        bigint,
+    to_map_copy_id   bigint,
 
 	FOREIGN KEY (from_area_id, from_map_id, from_map_copy_id) REFERENCES game.area(id, map_id, map_copy_id),
 	FOREIGN KEY (to_area_id, to_map_id, to_map_copy_id) REFERENCES game.area(id, map_id, map_copy_id),
@@ -175,26 +184,26 @@ CREATE TABLE game.link_short
 
 CREATE TABLE game.link_long
 (
-	from_map_id BIGINT REFERENCES game.map(id),
-	to_map_id BIGINT  REFERENCES game.map(id),
-	PRIMARY KEY (from_map_id, to_map_id)
+	from_map_id bigint REFERENCES game.map(id),
+	to_map_id   bigint  REFERENCES game.map(id),
+	PRIMARY     KEY (from_map_id, to_map_id)
 );
 
 CREATE TABLE game.object_type
 (
-	id SERIAL PRIMARY KEY,
-	image_link TEXT,
-	effect_id BIGINT REFERENCES game.effect(id)
+	id         serial PRIMARY KEY,
+	image_link text,
+	effect_id  bigint REFERENCES game.effect(id)
 );
 
 CREATE TABLE game.object
 (
-	id BIGSERIAL,
-	object_type_id INTEGER REFERENCES game.object_type(id),
-	area_id BIGINT,
-    map_id BIGINT,
-    map_copy_id BIGINT,
-    orientation FLOAT,
+	id             bigserial,
+	object_type_id integer REFERENCES game.object_type(id),
+	area_id        bigint,
+    map_id         bigint,
+    map_copy_id    bigint,
+    orientation    float,
 
 	FOREIGN KEY (area_id, map_id, map_copy_id) REFERENCES game.area(id, map_id, map_copy_id),
 	PRIMARY KEY (id, object_type_id)
@@ -202,46 +211,46 @@ CREATE TABLE game.object
 
 CREATE TABLE game.item
 (
-	id BIGSERIAL PRIMARY KEY,
-	name TEXT,
-	description TEXT,
-	effect_array TEXT
+	id           bigserial PRIMARY KEY,
+	name         text,
+	description  text,
+	effect_array text
 );
 
 CREATE TABLE game.dropped_item
 (
-	id BIGSERIAL PRIMARY KEY,
-	item_id BIGINT REFERENCES game.item(id),
-	area_id BIGINT
+	id      bigserial PRIMARY KEY,
+	item_id bigint REFERENCES game.item(id),
+	area_id bigint
 );
 
 CREATE TABLE game.prefix
 (
-	id BIGSERIAL PRIMARY KEY,
-	name TEXT,
-	effect_array TEXT
+	id           bigserial PRIMARY KEY,
+	name         text,
+	effect_array text
 );
 
 CREATE TABLE game.postfix
 (
-	id BIGSERIAL PRIMARY KEY,
-	name TEXT,
-	effect_array TEXT
+	id           bigserial PRIMARY KEY,
+	name         text,
+	effect_array text
 );
 
 CREATE TABLE game.item_instance
 (
-	id BIGSERIAL,
-	item_id BIGINT REFERENCES game.item(id),
-	character_id BIGINT REFERENCES game.character(id),
-	prefix_id BIGINT REFERENCES game.prefix(id),
-	postfix_id BIGINT REFERENCES game.postfix(id),
-	equipped BOOLEAN
+	id           bigserial,
+	item_id      bigint REFERENCES game.item(id),
+	character_id bigint REFERENCES game.character(id),
+	prefix_id    bigint REFERENCES game.prefix(id),
+	postfix_id   bigint REFERENCES game.postfix(id),
+	equipped     boolean
 );
 
 CREATE TABLE game.allowed_items
 (
-	item_id BIGINT REFERENCES game.item(id),
-	game_id BIGINT REFERENCES game.game(id),
+	item_id bigint REFERENCES game.item(id),
+	game_id bigint REFERENCES game.game(id),
 	PRIMARY KEY (item_id, game_id)
 );
